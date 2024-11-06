07f54375f832f474c22eba462bb66e7e40ca70c6
ardhi <ardhidhani@gmail.com>
2024-11-06 20:43:30 +0700

    fix: typo, wrong declared format, & two validate link update & create
    - fix typo in accesskey
    - fix declare validate format error delete space between validate
    - add two validating link for update and create

--- models/publik_link.go
+++ models/publik_link.go
@@ -8,7 +8,7 @@ type PublicLink struct {
 	// PublicPath   string `json:"public_path"`
 	RealRootBucket string `json:"real_root_bucket"`
 	RealRootPath   string `json:"real_root_path"`
-	AccessKey      string `json:"acccess_key"`
+	AccessKey      string `json:"access_key"`
 	AccessType     string `json:"access_type"`
 	Privacy        string `json:"privacy"`
 	CreatedAt      time.Time
@@ -22,10 +22,10 @@ type Security struct {
 }
 
 type PayloadPublicLink struct {
-	Link           string `json:"link" validate:"required, link"`
-	RealRootBucket string `json:"real_root_bucket" validate:"required, bucket"`
-	RealRootPath   string `json:"real_root_path" validate:"required, path"`
-	AccessKey      string `json:"acccess_key" validate:"accesskey"`
+	Link           string `json:"link" validate:"required,link"`
+	RealRootBucket string `json:"real_root_bucket" validate:"required,bucket"`
+	RealRootPath   string `json:"real_root_path" validate:"required,path"`
+	AccessKey      string `json:"access_key" validate:"accesskey"`
 	AccessType     string `json:"access_type"  validate:"required,accesstype"`
 	Privacy        string `json:"privacy"  validate:"required,privacy"`
 }
@@ -33,4 +33,13 @@ type PayloadPublicLink struct {
 type Alert struct {
 	Type    string
 	Message string
+}
+
+type ValidateLinkUpdate struct {
+	ID   string `json:"id" validate:"required"`
+	Link string `json:"link" validate:"required,link"`
+}
+
+type ValidateLinkCreate struct {
+	Link string `json:"link" validate:"required,link"`
 }
