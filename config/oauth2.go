79031bb68d4bd50b2e793cde80f74b1d58c106f3
Zenk41 <ardhidhani@gmail.com>
2025-01-24 11:01:57 +0700

    Refactor Google OAuth Configuration
    - Removed dependency on `viper` and `os` for environment variable loading in `GoogleConfig`.
    - Simplified `GoogleConfig` function to accept parameters:
      - `clientID`: Google Client ID.
      - `clientSecret`: Google Client Secret.
      - `registerCallback`: Redirect URL for Google OAuth registration callback.
      - `loginCallback`: Redirect URL for Google OAuth login callback.
    - Improved flexibility by allowing dynamic configuration through function arguments.
    - Retained the existing structure of `oauth2.Config` for `GoogleOauthConfigRegister` and `GoogleOauthConfigLogin`.
    
    This refactor improves clarity, testability, and reusability by decoupling environment-specific dependencies.

--- config/oauth2.go
+++ config/oauth2.go
@@ -1,10 +1,7 @@
 package config
 
 import (
-	"fmt"
-	"os"
 
-	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
@@ -16,25 +13,26 @@ type Config struct {
 
 var AppConfig Config
 
-func GoogleConfig(v *viper.Viper) {
-
-	// Debug: Print loaded environment variables
-	fmt.Println("GOOGLE_CLIENT_ID in config:", v.GetString("GOOGLE_CLIENT_ID"))
-	fmt.Println("GOOGLE_CLIENT_SECRET in config:", v.GetString("GOOGLE_CLIENT_SECRET"))
-
+func GoogleConfig(clientID, clientSecret, registerCallback, loginCallback string) {
 	AppConfig.GoogleOauthConfigRegister = oauth2.Config{
-		RedirectURL:  fmt.Sprintf("http://localhost:%s/api/oauth/register/callback", os.Getenv("PORT")),
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
-		Endpoint:     google.Endpoint,
+		RedirectURL:  registerCallback,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
 	}
 
 	AppConfig.GoogleOauthConfigLogin = oauth2.Config{
-		RedirectURL:  fmt.Sprintf("http://localhost:%s/api/oauth/login/callback", os.Getenv("PORT")),
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
-		Endpoint:     google.Endpoint,
+		RedirectURL:  loginCallback,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
 	}
 }
